8865ed8566d790f9b6beee19fb5e1892c00e46eb
ChosenQuill <31707957+ChosenQuill@users.noreply.github.com>
2019-12-16 01:49:26 -0600

    Prints outputs + Fails on Error + Changed To Bin/Bash

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+set -ev
 
 openssl aes-256-cbc -K $encrypted_18bade7c6277_key -iv $encrypted_18bade7c6277_iv -in travis_id_rsa.enc -out travis_id_rsa -d
 chmod 400 travis_id_rsa
 
 rsync -Ihvz -e "ssh -i ./travis_id_rsa" $TRAVIS_BUILD_DIR/build/libs/SureBot.jar auto@server.suredroid.com:/opt/discord/surebot/
 
-ssh auto@server.suredroid.com -i ./travis_id_rsa -T << EOF
+ssh auto@server.suredroid.com -i ./travis_id_rsa /bin/bash << EOF
   if screen -list | grep -q "discord"; then
     screen -X -S "discord" quit
   fi
