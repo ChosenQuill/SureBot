7da5b9386b8f085e29a7b4a717dd9beabc6088cf
SureDroid <support@suredroid.com>
2019-12-16 03:03:17 -0600

    Uploads only if newer for screen.conf + updated screen command

--- deploy.sh
+++ deploy.sh
@@ -9,12 +9,12 @@ rsync -Ihvz -e "ssh -i ./travis_id_rsa" $TRAVIS_BUILD_DIR/build/libs/SureBot.jar
 echo "multiuser on" >> screen.conf
 echo "acladd root" >> screen.conf
 
-rsync -Ihvz -e "ssh -i ./travis_id_rsa" $TRAVIS_BUILD_DIR/screen.conf auto@server.suredroid.com:/opt/discord/surebot/
+rsync -hvz -e "ssh -i ./travis_id_rsa" $TRAVIS_BUILD_DIR/screen.conf auto@server.suredroid.com:/opt/discord/surebot/
 
 ssh auto@server.suredroid.com -i ./travis_id_rsa /bin/bash << 'EOT'
   if screen -list | grep -q "discord"; then
     screen -X -S "discord" quit
   fi
   screen -dmS discord -c /opt/discord/surebot/screen.conf
-  screen -S discord -X stuff java -jar /opt/discord/surebot/SureBot.jar
+  screen -S discord -p 0 -X stuff "java -jar /opt/discord/surebot/SureBot.jar^M"
 EOT
