9bef3d7fc5e5d8fdea5e3f00f071df8ed24e2a39
SureDroid <support@suredroid.com>
2019-12-16 02:20:33 -0600

    Addes MultiUser Config

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,12 @@ openssl aes-256-cbc -K $encrypted_18bade7c6277_key -iv $encrypted_18bade7c6277_i
 chmod 400 travis_id_rsa
 
 rsync -Ihvz -e "ssh -i ./travis_id_rsa" $TRAVIS_BUILD_DIR/build/libs/SureBot.jar auto@server.suredroid.com:/opt/discord/surebot/
+rsync -Ihvz -e "ssh -i ./travis_id_rsa" $TRAVIS_BUILD_DIR/screen.conf auto@server.suredroid.com:/opt/discord/surebot/
 
-ssh auto@server.suredroid.com -i ./travis_id_rsa /bin/bash << EOF
+ssh auto@server.suredroid.com -i ./travis_id_rsa /bin/bash << 'EOT'
   if screen -list | grep -q "discord"; then
     screen -X -S "discord" quit
   fi
-  screen -dmS discord
+  screen -dmS discord -c /opt/discord/surebot/screen.conf
   screen -S discord -X stuff java -jar /opt/discord/surebot/SureBot.jar
-EOF
+EOT
